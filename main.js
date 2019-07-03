1b873ec48443a5dd53993de22f99b138033a3658
Adithya Paramasivam <aparamasivam@commvault.com>
2019-07-03 16:38:13 -0400

    Get item id from name

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ past_week = '18SS19'
 async function get_stock() {
   try {
     const response = await axios.get('https://www.supremenewyork.com/mobile_stock.json');
-    let stock = response.data.products_and_categories;
-    const release = response.data.release_week;
-    console.log((release != past_week));
+    let stock = response.data.products_and_categories.new;
+    // const release = response.data.release_week;
+    // console.log((release != past_week)); // return whether this week's release
     return stock;
   } catch(error) {
     console.log(error)
@@ -18,5 +18,22 @@ async function get_stock() {
   }
 }
 
-stock = get_stock()
-// console.log(stock)
+function find_and_add(keyword, products) {
+  let id = false;
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].name.toLowerCase().includes(keyword)) {
+      console.log(`${products[i].name} and ${products[i].id}`);
+      id = products[i].id;
+    }
+  }
+  if (id) {
+    console.log(`https://www.supremenewyork.com/shop/${id}`);
+  } else { console.log(`${keyword} could not be found`) }
+}
+
+async function full_purchase() {
+  const products = await get_stock();
+  setTimeout(() => find_and_add('foulard', products));
+}
+
+full_purchase();
